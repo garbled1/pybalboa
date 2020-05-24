c700805ff09c9fd295dcdb493fa4ef4c12d75d93
garbled1 <root@garbled.net>
2020-05-24 07:10:05 -0700

    Bump to 0.6 for fix to tmin/tmax

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='pybalboa',
-      version='0.5',
+      version='0.6',
       description='Module to communicate with a Balboa spa wifi adapter',
       long_description='Module to communicate with a Balboa spa wifi adapter',
       url='https://github.com/garbled1/pybalboa',
